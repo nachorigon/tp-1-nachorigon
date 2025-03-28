1c8cd1bd2f6dbdaf3e29fea395161e4445b6615d
nachorigon <nacho.rigon05@gmail.com>
2025-03-28 11:55:20 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
-    a = 57
-    b = 7
+  a = 57
+  b = 7
+  suma = a+b
+  resta = a-b
+  producto = a*b
+  promedio = (a+b)/2
+  division_entero = a//b
+  resto = a%b
+  division_decimal = a/b
+  print(suma)
+  print(resta)
+  print(producto)
+  print(promedio)
+  print(division_entero)
+  print(resto)
+  print(division_decimal)
